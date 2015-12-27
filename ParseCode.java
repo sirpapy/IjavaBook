5e2ea014d6515b5f60602c424300bf929f6d8acc
mauganLG <mauganLG@users.noreply.github.com>
2015-12-27 12:20:15 +0100

    Create ParseCode.java

---
+++ ParseCode.java
@@ -0,0 +1,25 @@
+package Execise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jdk.jshell.JShell;
+import jdk.jshell.SourceCodeAnalysis.CompletionInfo;
+
+public class ParseCode {
+
+	public static List<String> parseSourceCode(String source) {
+
+		ArrayList<String> listCode = new ArrayList<>();
+
+		try (JShell jshell = JShell.create()) {
+			CompletionInfo info = jshell.sourceCodeAnalysis().analyzeCompletion(source);
+			listCode.add(info.source);
+			for (; info.remaining != "";) {
+				info = jshell.sourceCodeAnalysis().analyzeCompletion(info.remaining);
+				listCode.add(info.source);
+			}
+		}
+		return listCode;
+	}
+}
